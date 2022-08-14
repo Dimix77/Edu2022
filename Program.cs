4da941a9045d2631f8cfa8c2d078c9bfa0d9a8ba
Dimix77 <weryluckyday77@gmail.com>
2022-08-14 19:09:25 +0800

    Save code,one problems

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programm
+{
+    static void SelectRase()
+    {
+        string[] namesRase = {"Peoples", "Elfs", "Gnomes", "Orks"};
+        for (int i = 0; i < namesRase.Length -1; i++)
+        {
+            Console.Write(i+1 + " - " + namesRace[i] + ", ");
+        }
+    }
+    static void Main(string[] args)
+
+   
+   {
+    Console.WriteLine();
+   }
+
+   }
+
+
