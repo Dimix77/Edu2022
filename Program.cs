1ccd00201d79ededa398cb8e495029f58c1ea486
Dimix77 <weryluckyday77@gmail.com>
2022-08-30 16:38:44 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
 ﻿{
-{
-static void main(string[] args);
-
-Console.Write("Enter number");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number > -100 && number < 100)
-{
-    Console.WriteLine("Third digital not");
-}
-else 
-{
-    if (number < 0)
     {
-        number = - 1 * number;
+        static void main(string[] args);
+
+        Console.Write("Enter number");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > -100 && number < 100)
+        {
+            Console.WriteLine("Third digital not");
+        }
+        else
+        {
+            if (number < 0)
+            {
+                number = -1 * number;
+            }
+            string thirdDigit = Convert.ToString(number);
+            Console.WriteLine(thirdDigit[2]);
+        }
     }
-    string thirdDigit = Convert.ToString(number);
-    Console.WriteLine(thirdDigit[2]);
-}
-}
 }
