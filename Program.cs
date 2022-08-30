d8333d157e3086ac63c48184127c0d0a2127b40b
Dimix77 <weryluckyday77@gmail.com>
2022-08-30 16:34:31 +0800

    End vers

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 ﻿{
-{
-static void main(string[] args);
 
-Console.Write("Enter number");
-int number = Convert.ToInt32(Console.ReadLine());
+    static void main(string[] args);
 
-if (number > -100 && number < 100)
-{
-    Console.WriteLine("Third digital not");
-}
-else 
-{
-    if (number < 0)
+    Console.WriteLine("Enter number, rot week day");
+    Console.WriteLine(" 1 - Mo; 2 - Tue; 3 - We; 4 - Th; 5 - Fr; 6 - Sat; 7 - Sun ");
+
+    int numberWeekDay = Convert.ToInt32(Console.ReadLine());
+    if (numberWeekDay == 6) (numberWeekDay == 7);
     {
-        number = - 1 * number;
+        Console.WriteLine("Yes");
     }
-    string thirdDigit = Convert.ToString(number);
-    Console.WriteLine(thirdDigit[2]);
-}
-}
+
+else
+
+    {
+        Console.WriteLine("No");
+    }
 }
